428e39effeba99caa910717ed412eb6006d83a5b
Daniel Cook <dcook63@students.kennesaw.edu>
2019-03-19 20:21:59 -0400

    Added way to submit form for specific student. Updated presentation form. Added database connection for presentation form.

--- MockupGUI/EvaluationForms/FormData.cs
+++ MockupGUI/EvaluationForms/FormData.cs
@@ -7,6 +7,7 @@ namespace MockupGUI
 {
     public class FormData
     {
+        //Peer Review
         public int _Contribution;
         public int Contribution
         {
@@ -103,42 +104,202 @@ namespace MockupGUI
                 _General_Comment = value;
             }
         }
-        public int _Overall;
-        public int Overall
+        public int _Overall_Peer;
+        public int Overall_Peer
         {
             get
             {
-                return _Overall;
+                return _Overall_Peer;
             }
             set
             {
-                _Overall = value;
+                _Overall_Peer = value;
             }
         }
-        public string _Overall_Comment;
-        public string Overall_Comment
+        public string _Overall_Peer_Comment;
+        public string Overall_Peer_Comment
         {
             get
             {
-                return _Overall_Comment;
+                return _Overall_Peer_Comment;
             }
             set
             {
-                _Overall_Comment = value;
+                _Overall_Peer_Comment = value;
             }
         }
-        public void FillData(int cont, string cont_com, int comm, string comm_com, int team, string team_com, int gen, string gen_com, int ovr, string ovr_com)
+
+        //Presentation_Eval
+        public int _Poster;
+        public int Poster
         {
-            Contribution = cont;
-            Contribution_Comment = cont_com;
+            get
+            {
+                return _Poster;
+            }
+            set
+            {
+               _Poster = value;
+            }
+        }
+        public string _Poster_Comment;
+        public string Poster_Comment
+        {
+            get
+            {
+                return _Poster_Comment;
+            }
+            set
+            {
+                _Poster_Comment = value;
+            }
+        }
+        public int _Teamtalk;
+        public int Teamtalk
+        {
+            get
+            {
+                return _Teamtalk;
+            }
+            set
+            {
+                _Teamtalk = value;
+            }
+        }
+        public string _Teamtalk_Comment;
+        public string Teamtalk_Comment
+        {
+            get
+            {
+                return _Teamtalk_Comment;
+            }
+            set
+            {
+                _Teamtalk_Comment = value;
+            }
+        }
+        public int _Slides;
+        public int Slides
+        {
+            get
+            {
+                return _Slides;
+            }
+            set
+            {
+                _Slides = value;
+            }
+        }
+        public string _Slides_Comment;
+        public string Slides_Comment
+        {
+            get
+            {
+                return _Slides_Comment;
+            }
+            set
+            {
+                _Slides_Comment = value;
+            }
+        }
+        public int _Presentation;
+        public int Presentation
+        {
+            get
+            {
+                return _Presentation;
+            }
+            set
+            {
+                _Presentation = value;
+            }
+        }
+        public string _Presentation_Comment;
+        public string Presentation_Comment
+        {
+            get
+            {
+                return _Presentation_Comment;
+            }
+            set
+            {
+                _Presentation_Comment = value;
+            }
+        }
+        public int _Deliverables;
+        public int Deliverables
+        {
+            get
+            {
+                return _Deliverables;
+            }
+            set
+            {
+                _Deliverables = value;
+            }
+        }
+        public string _Deliverables_Comment;
+        public string Deliverables_Comment
+        {
+            get
+            {
+                return _Deliverables_Comment;
+            }
+            set
+            {
+                _Deliverables_Comment = value;
+            }
+        }
+        public int _Softskills;
+        public int Softskills
+        {
+            get
+            {
+                return _Softskills;
+            }
+            set
+            {
+                _Softskills = value;
+            }
+        }
+        public string _Softskills_Comment;
+        public string Softskills_Comment
+        {
+            get
+            {
+                return _Softskills_Comment;
+            }
+            set
+            {
+                _Softskills_Comment = value;
+            }
+        }
+        public int _Overall_Pres;
+        public int Overall_Pres
+        {
+            get
+            {
+                return _Overall_Pres;
+            }
+            set
+            {
+                _Overall_Pres = value;
+            }
+        }
+
+
+        public void FillPeerData(int cont, string cont_com, int comm, string comm_com, int team, string team_com, int gen, string gen_com, int ovr, string ovr_com)
+        {
+            Overall_Peer = cont;
+            Overall_Peer_Comment = cont_com;
             Communication = comm;
             Communication_Comment = comm_com;
             Teamwork = team;
             Teamwork_Comment = team_com;
             General = gen;
             General_Comment = gen_com;
-            Overall = ovr;
-            Overall_Comment = ovr_com;
+            Overall_Peer = ovr;
+            Overall_Peer_Comment = ovr_com;
         }
     }
 }
